e07f1fad5a71f462774cfd91ab002d62e916bff5
AlanZ <alanzheng2004@outlook.com>
2025-04-09 19:57:46 -0400

    working on creating multiple ports

--- src/thread/read.rs
+++ src/thread/read.rs
@@ -1,13 +1,17 @@
 // pub mod thread;
 
 use tokio::net::TcpStream;
+use std::borrow::Cow;
 use std::sync::Arc;
 use tokio::sync::Mutex;
 use tokio::io::AsyncReadExt;
 use tokio::io::Interest;
 
 
+
 pub async fn read_from_stream(stream: Arc<Mutex<TcpStream>>) -> (){
+    let stream_ip = stream.lock().await.peer_addr().unwrap().ip().to_string();
+    println!("IP TEST:{}",stream_ip);
     loop{
         let mut read_lock = stream.lock().await;
         match read_lock.ready(Interest::READABLE).await{
@@ -20,7 +24,47 @@ pub async fn read_from_stream(stream: Arc<Mutex<TcpStream>>) -> (){
                         if(n>0)
                         {
                             println!("Read from {}:{}",read_lock.local_addr().unwrap(),String::from_utf8_lossy(&buf));
+                            // println!("{TEST}");
+                            if String::from_utf8_lossy(&buf).contains("FOLDER"){
+                                println!("TEST");
+                                read_lock.read(&mut buf);
+                                let string_buf = String::from_utf8_lossy(&buf);
+                                if(string_buf.contains("PORT")){
+                                    let cloned_string: Cow<'_,str>= Cow::Owned(string_buf.to_string());
+                                    //spawn a thread for each port connection
+
+                                    tokio::spawn(async {
+                                        parse_file_per_port(cloned_string.into_owned()).await;
+                                    });
+                                }
+
+                            }
                         }
+                    }
+                    // Ok(_) => {break;}
+                    Err(e) => {eprintln!("Error: {}",e);}
+                }
+            }
+            Err(_) => {}
+        }
+    }
+}
+
+pub async fn parse_file_per_port(port: String){
+    // TcpStream::connect(stream.)
+}
+
+pub async fn read_to_file(stream: Arc<Mutex<TcpStream>>) -> (){
+    loop{
+        let mut read_lock = stream.lock().await;
+        match read_lock.ready(Interest::READABLE).await{
+            Ok(_) => {
+                let mut buf =[0;4096];
+                // if(read_lock.poll_read_ready(cx))
+                
+                match read_lock.read(&mut buf).await{
+                    Ok(n) => {
+                        
                     }
                     // Ok(_) => {break;}
                     Err(e) => {eprintln!("Error: {}",e);}
--- src/thread/write.rs
+++ src/thread/write.rs
@@ -1,10 +1,13 @@
+use core::num;
 use std::fs::File;
 use std::sync::Arc;
 use tokio::sync::Mutex;
-use tokio::net::TcpStream;
+use tokio::net::{TcpListener, TcpStream};
 use rfd::FileDialog;
 use tokio::fs;
 use tokio::io::AsyncWriteExt;
+
+use crate::main;
 
 
 pub async fn write_a_file(stream: Arc<Mutex<TcpStream>>) -> (){
@@ -34,9 +37,74 @@ pub async fn write_a_file(stream: Arc<Mutex<TcpStream>>) -> (){
 }
 
 pub async fn write_a_folder(stream: Arc<Mutex<TcpStream>>) -> (){
+    let num_of_ports = 10;
+    //creating ports
+    let mut available_ports:Vec<u16> = vec![];
+    for i in 0..num_of_ports{
+        let sub_listener = TcpListener::bind("0.0.0.0:0").await.unwrap();
+        let port = sub_listener.local_addr().unwrap().port();
+        available_ports.push(port);
+    }
+
+    //writing list of ports to other device
+    let mut main_stream_lock = stream.lock().await;
+    let _ = main_stream_lock.write_all(b"FOLDER\n").await;
+    for port in available_ports{
+        let info = format!("PORT {}\n",port);
+        let _ = main_stream_lock.write_all(info.as_bytes()).await;
+    }
+    let _ = main_stream_lock.write_all(b"END\n").await;
+
+
     if let Some(file_path) = FileDialog::new().set_directory("/".to_string()).pick_folder(){
         println!("folder: {}",file_path.display());
+        if let Ok(mut files) = fs::read_dir(file_path).await{
+            let mut i = 1;
+            loop{
+                let result = files.next_entry().await;
+                match result{
+                    Ok(file) => {
+                        println!("File #{} : {:?}",i,file.unwrap().file_name());
+
+                    }
+                    Err(_) => println!("No more files")
+                }
+                i+=1;
+            } 
+        }
     } else{
         println!("No folder.")
     }
+}
+
+
+pub async fn write_file(stream: Arc<Mutex<TcpStream>>) -> (){
+    let mut file_path = String::new();
+    // let mut file = File::create_new("");
+    let mut buffer = String::new();
+    // }
+    let file_path = FileDialog::new().set_directory("/".to_string()).pick_file();
+    // let content = file.unwrap();
+    let mut file_str = file_path.clone().unwrap();
+
+
+    let file_name = file_str.file_name().unwrap().to_str().unwrap();
+    let file_name_len = file_name.len() as u32; 
+
+    let mut file_str = file_str.to_str().unwrap();
+    println!("test:{}",file_str);
+
+    let mut content: Vec<u8> = Vec::new();
+    match fs::read(file_str).await{
+        Ok(result) => {content = result;},
+        Err(e) => eprintln!("ERROR:{}",e),
+    }
+    let mut content_str = String::from_utf8_lossy(&content);
+    
+    let mut stream_lock = stream.lock().await;
+
+    stream_lock.write_all(&content).await;
+    // stream_lock.write(b"test").await;
+
+    println!("test:{}",content_str.as_ref());
 }
