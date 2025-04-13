3b50014d9c543efecd402d897b4a2a66fd2174a8
AlanZ <alanzheng2004@outlook.com>
2025-04-13 02:10:33 -0400

    test

--- src/thread/read.rs
+++ src/thread/read.rs
@@ -160,9 +160,8 @@ pub async fn read_from_stream(stream: Arc<Mutex<TcpStream>>, outgoing_adder:Stri
     }
 }
 
-pub async fn read_file_from_stream_no_async(mut stream: TcpStream,folder_path:Option<String>){
+pub async fn read_file_from_stream_no_async(mut stream: Arc<Mutex<TcpStream>>,folder_path:Option<String>){
     let folder_path = folder_path.clone();
-    // let mut read_lock = stream.lock().await;
     
 
 
@@ -175,11 +174,11 @@ pub async fn read_file_from_stream_no_async(mut stream: TcpStream,folder_path:Op
     //         return FileDialog::new().set_title("Choose save location").set_directory("/".to_string()).pick_folder().unwrap();
     //     }).await.unwrap();
     // }
+    // let mut stream_lock = stream.lock();
     
     loop{
         line.clear();
-        let mut buf_reader = BufReader::new(&mut stream);
-        match buf_reader.read_line(&mut line).await{
+        match read_line_from_stream(stream.clone(),&mut line).await{
             Ok(0) =>{
                 println!("CLOSED");
                 break;
@@ -190,13 +189,13 @@ pub async fn read_file_from_stream_no_async(mut stream: TcpStream,folder_path:Op
                 //reading a single file
                 if line.contains("FILE"){
                     line.clear();
-                    buf_reader.read_line(&mut line).await;
+                    read_line_from_stream(stream.clone(),&mut line).await;
                     let cloned_line = line.clone();
                     let file_name = cloned_line.strip_prefix("FILENAME:").unwrap().trim();
                     println!("FILENAME:{}", file_name);
 
                     line.clear();
-                    buf_reader.read_line(&mut line).await;
+                    read_line_from_stream(stream.clone(),&mut line).await;
                     let file_size = line.strip_prefix("FILESIZE:").unwrap().trim();
                     println!("FILESIZE:{}", file_size);
 
@@ -212,10 +211,10 @@ pub async fn read_file_from_stream_no_async(mut stream: TcpStream,folder_path:Op
                         println!("FOLDER PATH:{}",file_path.to_str().unwrap());
                         file = File::create(file_path).await.unwrap();
                     }
-                    drop(buf_reader);
                     let mut buffer = [0u8; 4096];
                     while received < file_size_usize{
-                        let n = stream.read(&mut buffer).await.unwrap();
+                        
+                        let n = stream.lock().await.read(&mut buffer).await.unwrap();
 
                         if n == 0{
                             break;
@@ -239,7 +238,7 @@ pub async fn parse_file_per_port(address: String, folder_path:Option<String>){
     // print
     match TcpStream::connect(address.trim()).await{
         Ok(stream)=>{
-            // let stream: Arc<Mutex<TcpStream>> = Arc::new(Mutex::new(stream));
+            let stream: Arc<Mutex<TcpStream>> = Arc::new(Mutex::new(stream));
             
             println!("connected to port");
             read_file_from_stream_no_async(stream, folder_path).await;
